7bd65b9d040ea286b6643a19f585ac406f84af64
minh <minh@10.33.48.153>
2023-04-19 15:18:33 +0700

    Initialize the code for personal banking program

---
+++ src/java/Controller/LoginController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import jakarta.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author minh
+ */
+public class LoginController extends HttpServlet{
+    
+}
